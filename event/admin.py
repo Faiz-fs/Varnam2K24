5bcccd481ba5e341adddea017408c21ab91ed4be
Faiz-fs <118742111+Faiz-fs@users.noreply.github.com>
2024-04-02 12:53:29 +0530

    Update admin.py

--- event/admin.py
+++ event/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import EventRegist
+from .models import EventRegist,GrupRegist
 
 # Register your models here.
 
 admin.site.register(EventRegist)
+admin.site.register(GrupRegist)
