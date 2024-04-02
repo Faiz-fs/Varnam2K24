c4ca05ddce3d6e2ffbd7443d081210e4a9c342cc
Faiz-fs <118742111+Faiz-fs@users.noreply.github.com>
2024-04-02 12:52:46 +0530

    Update models.py

--- event/models.py
+++ event/models.py
@@ -11,3 +11,16 @@ class EventRegist(models.Model):
     
     def __str__(self) -> str:
         return self.email
+
+class GrupRegist(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    phoneno=models.CharField(max_length=10)
+    teamname=models.CharField(max_length=50)
+    eventname=models.CharField(max_length=50)
+    
+    
+    def __str__(self) -> str:
+        return self.email
+
